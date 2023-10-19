7a28e6c497403b990790c8fb7d6d8dbe054892f6
PAU COLOME, Vodafone <pau.colome@vodafone.com>
2023-10-19 08:34:01 +0200

    feat: removed code

--- packages/firejs/src/types.ts
+++ packages/firejs/src/types.ts
@@ -10,29 +10,4 @@ export type JSXElement<P extends Props = Props> = {
 	props: P & { children: JSXElement[]; nodeValue?: string };
 };
 
-export interface FunctionComponent<P extends Props = Props> {
-	(props: P): JSXElement;
-}
 
-export function isFunctionComponent<P extends Props = Props>(
-	element: FireElementType,
-): element is FunctionComponent<P> {
-	return element instanceof Function;
-}
-
-export type Hook = {
-	state: any;
-	queue: any[];
-};
-
-export type Fiber = {
-	type?: FireElementType;
-	props: Props & { children: JSXElement[]; nodeValue?: string };
-	dom: DOMElement;
-	parent?: Fiber;
-	child?: Fiber;
-	sibling?: Fiber;
-	alternate?: Fiber; // link to old fiber
-	effectTag?: "PLACEMENT" | "UPDATE" | "DELETION";
-	hooks?: Hook[];
-};
